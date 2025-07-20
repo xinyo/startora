83955ecd1fa7a0aa49ab6a97eed0ac28fbdcd3dd
xinyo <xinyoc@gmail.com>
2025-07-20 19:37:17 +1000

    add files

---
+++ src/client/definition.ts
@@ -0,0 +1,20 @@
+export const colors = [
+  {
+    name: "default",
+    primary: "black",
+    accent: "gray",
+    background: "white",
+  },
+  {
+    name: "Blackboard",
+    primary: "#FFFDEA",
+    accent: "#5c5c5c",
+    background: "#1a1a1a",
+  },
+  {
+    name: "Monokai",
+    primary: "#d6d6d6",
+    accent: "#797979",
+    background: "#2e2e2e",
+  },
+];
