30ec9313d36a3a6a20aeff856ffa64136d2d0eb7
xinyo <xinyoc@gmail.com>
2026-07-27 21:18:34 +1000

    reduce size with webp for png

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,22 @@ import tailwindcss from "@tailwindcss/vite";
 import react, { reactCompilerPreset } from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    imagetools({
+      include: /[\\/]src[\\/]assets[\\/].*\.png(?:\?.*)?$/i,
+      defaultDirectives: new URLSearchParams({
+        w: "200",
+        h: "200",
+        fit: "inside",
+        format: "webp",
+        quality: "70",
+        effort: "max",
+      }),
+    }),
     react(),
     tailwindcss(),
     babel({ presets: [reactCompilerPreset()] }),
